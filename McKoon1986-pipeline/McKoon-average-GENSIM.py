87aaa49670452de8ddf80dea36ca0e38ef07187b
TakahisaUchida <31158071+TakahisaUchida@users.noreply.github.com>
2022-11-06 22:39:37 +0900

    Add files via upload

---
+++ McKoon1986-pipeline/McKoon-average-GENSIM.py
@@ -0,0 +1,185 @@
+from wikipedia2vec import Wikipedia2Vec
+import numpy as np
+import matplotlib.pyplot as plt
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+import csv
+
+import scipy
+from scipy import stats
+
+import gensim
+from gensim import models
+from gensim.models import Word2Vec
+
+import gensim.downloader as api
+
+
+def cos_sim(v1, v2):
+	return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+#nlp = gensim.models.KeyedVectors.load_word2vec_format('../pretrainedData/GoogleNews-vectors-negative300.bin', binary=True) #without *norm_only* param
+
+
+#pretrained_model = "fasttext-wiki-news-subwords-300"
+##pretrained_model = "conceptnet-numberbatch-17-06-300"
+##pretrained_model = "word2vec-ruscorpora-300"
+#pretrained_model = "word2vec-google-news-300"
+#pretrained_model = "glove-wiki-gigaword-50"
+pretrained_model = "glove-wiki-gigaword-100"
+#pretrained_model = "glove-wiki-gigaword-200"
+#pretrained_model = "glove-wiki-gigaword-300"
+#pretrained_model = "glove-twitter-25"
+#pretrained_model = "glove-twitter-50"
+#pretrained_model = "glove-twitter-100"
+#pretrained_model = "glove-twitter-200"
+
+nlp = api.load(pretrained_model)
+
+
+N = 32
+
+A = np.empty(N)
+
+E = np.empty(N)
+
+f = open('McKoon_experiment-'+pretrained_model+'.csv', 'w')
+writer = csv.writer(f, lineterminator='\n')
+
+for i in range(N):
+    print('\n############### ' + str(i + 1) + ' ###############')
+
+    # read txt file for data
+    f = open('./data_mckoon1986/'+str(i + 1)+'.txt', 'r')
+    list = f.readlines()
+    discourse_words_1 = list[0].split()
+    discourse_words_2 = list[1].split()
+    target_word_1 = list[2].lower()
+
+    f.close()
+
+    # large capital -> small capital
+    discourse_words_1 = [s.replace(s, s.lower()) for s in discourse_words_1]
+    discourse_words_2 = [s.replace(s, s.lower()) for s in discourse_words_2]
+
+    # remove '.' and ',' from word list
+    discourse_words_1 = [s.replace('.', '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace('.', '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace(',', '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace(',', '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace(';', '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace(';', '') for s in discourse_words_2]
+
+    # remove stop words from word list
+    stop_words = stopwords.words('english')
+    #print(stop_words)
+    for stop_word in stop_words:
+        while stop_word in discourse_words_1 :
+            discourse_words_1.remove(stop_word)
+            
+        while stop_word in discourse_words_2 :
+            discourse_words_2.remove(stop_word)
+            
+    # remove "'s" and "'" and "-" and "'d" and "'ll" and "'ve" and "re" from word list
+    discourse_words_1 = [s.replace("'s", '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace("'s", '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace("'", '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace("'", '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace("-", '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace("-", '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace("'d", '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace("'d", '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace("'ll", '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace("'ll", '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace("'ve", '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace("'ve", '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace("'re", '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace("'re", '') for s in discourse_words_2]
+
+    # replace '\n' from target words
+    target_word_1 = target_word_1.replace('\n', '')
+
+
+    print('Data:')
+    print('target_word_1: %s' % target_word_1)
+   
+
+    print('discourse_words_1:')
+    print(discourse_words_1)
+    print('discourse_words_2:')
+    print(discourse_words_2)
+
+
+    target_word_1_vector = nlp[target_word_1]
+ 
+    '''
+    fig, ax = plt.subplots()
+    t = np.linspace(1, 2, 2)
+    '''
+
+    trajectory_word_1 = np.array([])
+ 
+    print('\nStep1: ')
+
+    for num in range(len(discourse_words_1)):
+        #print(discourse_words_1[num])
+        if num == 0:
+            if discourse_words_1[num] in nlp:
+                discourse_vector_1 = nlp[discourse_words_1[num]]
+        else:
+            if discourse_words_1[num] in nlp:    
+                discourse_vector_1 = (num * discourse_vector_1 + nlp[discourse_words_1[num]]) / (num + 1)
+
+    print('cos(discourse_vector_1, %s)=%f' % (target_word_1, cos_sim(discourse_vector_1, target_word_1_vector)))
+
+    A[i] = cos_sim(discourse_vector_1, target_word_1_vector)
+    
+    trajectory_word_1 = np.append(trajectory_word_1, cos_sim(discourse_vector_1, target_word_1_vector))
+
+    print('\nStep2: ')
+
+    for num in range(len(discourse_words_2)):
+        if num == 0:
+            if discourse_words_2[num] in nlp:
+                discourse_vector_2 = nlp[discourse_words_2[num]]
+        else:
+            if discourse_words_2[num] in nlp:    
+                discourse_vector_2 = (num * discourse_vector_2 + nlp[discourse_words_2[num]]) / (num + 1)
+
+    print('cos(discourse_vector_2, %s)=%f' % (target_word_1, cos_sim(discourse_vector_2, target_word_1_vector)))
+    
+    E[i] = cos_sim(discourse_vector_2, target_word_1_vector)
+
+    writer.writerow([cos_sim(discourse_vector_1, target_word_1_vector),  cos_sim(discourse_vector_2, target_word_1_vector)])
+
+    trajectory_word_1 = np.append(trajectory_word_1, cos_sim(discourse_vector_2, target_word_1_vector))
+ 
+
+f.close()
+
+
+data_to_plot = [A,  E]
+
+print(data_to_plot)
+
+# Create a figure instance
+#fig = plt.figure(1, figsize=(9, 6))
+fig = plt.figure(1)
+# Create an axes instance
+ax = fig.add_subplot(111)
+
+# Create the boxplot
+bp = ax.boxplot(data_to_plot)
+
+ax.set_xticklabels(['A', 'E'])
+fig.tight_layout()
+plt.savefig('McKoon-86-avg-'+pretrained_model+'.png')
+plt.show()
+
+print('t-test REL: ', stats.ttest_rel(A, E))
+print('t-test IND: ', stats.ttest_ind(A, E))
+
+difference = A - E
+print(difference)
+
---
+++ McKoon1986-pipeline/McKoon-average-bert.py
@@ -0,0 +1,174 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+import csv
+
+import scipy
+from scipy import stats
+
+import spacy
+
+
+def cos_sim(v1, v2):
+	return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+#load the language model
+nlp = spacy.load('../pretrainedData/glove-twit-200')
+
+N = 32
+
+#data structures to store the results
+A = np.empty(N)
+
+E = np.empty(N)
+
+#data output file
+f = open('McKoon_experiment-twitter.csv', 'w')
+writer = csv.writer(f, lineterminator='\n')
+
+for i in range(N):
+    print('\n############### ' + str(i + 1) + ' ###############')
+
+    # read txt file for data
+    f = open('./data_mckoon1986/'+str(i + 1)+'.txt', 'r')
+    list = f.readlines()
+    discourse_words_1 = list[0].split()
+    discourse_words_2 = list[1].split()
+    target_word_1 = list[2].lower()
+
+    f.close()
+
+    # large capital -> small capital
+    discourse_words_1 = [s.replace(s, s.lower()) for s in discourse_words_1]
+    discourse_words_2 = [s.replace(s, s.lower()) for s in discourse_words_2]
+
+    # remove '.' and ',' from word list
+    discourse_words_1 = [s.replace('.', '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace('.', '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace(',', '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace(',', '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace(';', '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace(';', '') for s in discourse_words_2]
+
+    # remove stop words from word list
+    stop_words = stopwords.words('english')
+    #print(stop_words)
+    for stop_word in stop_words:
+        while stop_word in discourse_words_1 :
+            discourse_words_1.remove(stop_word)
+            
+        while stop_word in discourse_words_2 :
+            discourse_words_2.remove(stop_word)
+            
+    # remove "'s" and "'" and "-" and "'d" and "'ll" and "'ve" and "re" from word list
+    discourse_words_1 = [s.replace("'s", '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace("'s", '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace("'", '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace("'", '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace("-", '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace("-", '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace("'d", '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace("'d", '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace("'ll", '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace("'ll", '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace("'ve", '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace("'ve", '') for s in discourse_words_2]
+    discourse_words_1 = [s.replace("'re", '') for s in discourse_words_1]
+    discourse_words_2 = [s.replace("'re", '') for s in discourse_words_2]
+
+    # replace '\n' from target words
+    target_word_1 = target_word_1.replace('\n', '')
+
+
+    print('Data:')
+    print('target_word_1: %s' % target_word_1)
+   
+
+    print('discourse_words_1:')
+    print(discourse_words_1)
+    print('discourse_words_2:')
+    print(discourse_words_2)
+
+
+    target_word_1_vector = nlp(target_word_1).vector
+ 
+    '''
+    fig, ax = plt.subplots()
+    t = np.linspace(1, 2, 2)
+    '''
+
+    trajectory_word_1 = np.array([])
+ 
+    print('\nStep1: ')
+
+    # create the vector average of the discourse words
+    # this is where we would want to send the whole discourse_words_1 string to BERT
+    # probably need to correctly format the input to BERT
+
+    for num in range(len(discourse_words_1)):
+        #print(discourse_words_1[num])
+        if num == 0:
+            discourse_vector_1 = nlp(discourse_words_1[num]).vector
+        else:
+            discourse_vector_1 = (num * discourse_vector_1 + nlp(discourse_words_1[num]).vector) / (num + 1)
+
+    print('cos(discourse_vector_1, %s)=%f' % (target_word_1, cos_sim(discourse_vector_1, target_word_1_vector)))
+
+    A[i] = cos_sim(discourse_vector_1, target_word_1_vector)
+    
+    trajectory_word_1 = np.append(trajectory_word_1, cos_sim(discourse_vector_1, target_word_1_vector))
+
+    print('\nStep2: ')
+
+    # create the vector average of the discourse words
+    # this is where we would want to send the whole discourse_words_1 string to BERT
+    # probably need to correctly format the input to BERT
+
+
+    for num in range(len(discourse_words_2)):
+        #print(discourse_words_1and2[num])
+        if num == 0:
+            discourse_vector_2 = nlp(discourse_words_2[num]).vector
+        else:
+            discourse_vector_2 = (num * discourse_vector_2 + nlp(discourse_words_2[num]).vector) / (num + 1)
+
+
+    print('cos(discourse_vector_2, %s)=%f' % (target_word_1, cos_sim(discourse_vector_2, target_word_1_vector)))
+    
+    E[i] = cos_sim(discourse_vector_2, target_word_1_vector)
+
+    writer.writerow([cos_sim(discourse_vector_1, target_word_1_vector),  cos_sim(discourse_vector_2, target_word_1_vector)])
+
+    trajectory_word_1 = np.append(trajectory_word_1, cos_sim(discourse_vector_2, target_word_1_vector))
+ 
+
+f.close()
+
+
+data_to_plot = [A,  E]
+
+print(data_to_plot)
+
+# Create a figure instance
+#fig = plt.figure(1, figsize=(9, 6))
+fig = plt.figure(1)
+# Create an axes instance
+ax = fig.add_subplot(111)
+
+# Create the boxplot
+bp = ax.boxplot(data_to_plot)
+
+ax.set_xticklabels(['A', 'E'])
+fig.tight_layout()
+plt.savefig('McKoon-86-twitter.png')
+plt.show()
+
+print('t-test REL: ', stats.ttest_rel(A, E))
+print('t-test IND: ', stats.ttest_ind(A, E))
+
+difference = A - E
+#print(difference)
+
